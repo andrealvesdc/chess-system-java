b8643bf5840178f3a432933ff062126c19d1ee90
André Alves <andrealvesdc@gmail.com>
2019-09-30 16:26:21 -0300

    Boardexception and defensive programming

---
+++ src/boardgame/Boardexception.java
@@ -0,0 +1,10 @@
+package boardgame;
+
+public class Boardexception extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public Boardexception(String msg) {
+		super (msg);
+	}
+
+}
