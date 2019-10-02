1739cbbb1083a8911829f747fad1d7853c6e3242
André Alves <andrealvesdc@gmail.com>
2019-10-02 14:55:49 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,5 @@
 package chess;
 
-import javax.swing.border.Border;
-
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -32,9 +30,11 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPositoin.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
+	
 	
 	private Piece makeMove(Position source, Position target) {
 		Piece p = board.removePiece(source);
@@ -49,6 +49,12 @@ public class ChessMatch {
 		}
 		if (board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
+		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to target position");
 		}
 	}
 
